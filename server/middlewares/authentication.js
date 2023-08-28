55b7eb6fc9c66a333851c9551181e8e894921678
M-Talha-Zafar <talha.zafar@devsinc.com>
2023-08-28 13:36:30 +0500

    Removed log

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -7,7 +7,6 @@ const authenticateJwt = (req, res, next) => {
       return res.status(401).json({ message: "Unauthorized" });
     }
     if (!user) {
-      console.log("User doesn't exist");
       return res.status(401).json({ message: "Unauthorized" });
     }
     req.user = user;
